44877a5ad74832a1739da745e2a4972f56fdcc98
Ayman Mubarak <cs.aymanmubark@gmail.com>
2021-10-23 12:04:37 +0200

    ok

--- lib/photo_card_swiper.dart
+++ lib/photo_card_swiper.dart
@@ -262,7 +262,7 @@ class _PhotoCardSwiperState extends State<PhotoCardSwiper> {
                                 _likeCard(forIndex: _index);
                                 _hideAllPhotoCardOverlayWidgets();
                                 if (widget.rightButtonAction != null) {
-                                  widget.rightButtonAction!();
+                                  widget.rightButtonAction!(_index);
                                 }
                               });
                             },
